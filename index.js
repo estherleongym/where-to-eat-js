d2ef89df70b4d1065bd1c3a719ac019c7f0e7b69
Esther Leong Yuet Mei <estherleongyuetmei@Esthers-MacBook-Air.local>
2017-08-30 00:22:29 +0800

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 function addRestaurantListener(){
-  startBtn = document.getElementById('start-random-button');
+		startBtn = document.getElementById('start-random-button');
   
   // Replace 'functionName' with the name of the function that you have written
-  startBtn.addEventListener("click", functionName)
-
-  getBtn = document.getElementById('get-restaurant-button');
-  
-  // Replace 'functionName' with the name of the function that you have written
-  getBtn.addEventListener("click", functionName);
+		startBtn.addEventListener("click", startRandomizing);
 }
 
-// wait for DOM content to load before binding an event
+
+function startRandomizing(){
+
+getBtn = document.getElementById('get-restaurant-button');
+
+interval = setInterval(function(){ 
+	
+	choiceArray = ['Nasi Ayam Kunyit', 'Ayam Penyet', 'Teh Tarik Place', 'Subway','Rakuzen', 'Onde-Onde', 'Boat Noodles', 'Chicken Rice Shop', 'Nando\'s'];
+		displayChoice = document.getElementById('restaurant');
+			arrayLength = choiceArray.length;
+  			rand = getRandomInt(0, arrayLength);
+  				displayChoice.innerHTML = choiceArray[rand];
+ 
+ console.log(rand);}, 100)
+
+ getBtn.addEventListener("click", function(){
+
+    		clearInterval(interval);
+    		displayChoice.innerHTML;
+    		console.log(rand);
+
+  		})
+				
+
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
 document.addEventListener('DOMContentLoaded', addRestaurantListener);
